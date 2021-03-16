43102a8b918b8f17354fe4adbd237ed0d99adc66
Konstantinos Pantelios <konstantinospantelios@yahoo.com>
2021-03-16 12:29:25 +0100

    Resolved Triangulation - Need to add det

--- Triangulation/triangulation_method.cpp
+++ Triangulation/triangulation_method.cpp
@@ -55,10 +55,48 @@ Matrix<double> to_Matrix(const mat &M) {
 }
 
 
-/**
- * TODO: Finish this function for reconstructing 3D geometry from corresponding image points.
- * @return True on success, otherwise false. On success, the reconstructed 3D points must be written to 'points_3d'.
- */
+
+mat34 Rt_selection( mat3 R, vec3 t) {
+
+    mat34 Rt; // 3 by 4
+    Rt.set_col(0, R.col(0)); // R is the first column of Rt
+    Rt.set_col(1, R.col(1));
+    Rt.set_col(2, R.col(2));
+    Rt.set_col(3, t);
+
+    return Rt;
+}
+// ****************************** Triangulation function : returns the 3D Point **************************************//
+vec3 triangulation1(vec3 p0, vec3 p1, mat3 K, mat34 Rt, mat34 Rt0){
+    mat34   M1 = K*Rt0; // 3 by 4 M // for the second camera
+    mat34   M2 = K*Rt;
+
+    Matrix<double> A(4,4,0.0);
+    vec4 fr, sr,tr,lr;
+
+    fr = p0.x*M1.row(2) - M1.row(0);
+    sr = p0.y*M1.row(2) - M1.row(1);
+    tr = p1.x*M2.row(2) - M2.row(0);
+    lr = p1.y*M2.row(2) - M2.row(1);
+    A.set_row({fr[0],fr[1],fr[2],fr[3]},0);
+    A.set_row({sr[0],sr[1],sr[2],sr[3]},1);
+    A.set_row({tr[0],tr[1],tr[2],tr[3]},2);
+    A.set_row({lr[0],lr[1],lr[2],lr[3]},3);
+
+    Matrix<double> Ua(4, 4, 0.0);   // initialized with 0s
+    Matrix<double> Sa(4, 4, 0.0);   // initialized with 0s
+    Matrix<double> Va(4, 4, 0.0);   // initialized with 0s
+
+    svd_decompose(A, Ua, Sa, Va);
+
+    vec4 P;
+    P = vec4(Va.get_column(Va.cols()-1).data()); //read data from std::vector
+
+    vec3 P_homogeneuous = vec3(P[0]/P[3], P[1]/P[3], P[2]/P[3]);
+
+    return P_homogeneuous;
+}
+
 bool Triangulation::triangulation(
         float fx, float fy,     /// input: the focal lengths (same for both cameras)
         float cx, float cy,     /// input: the principal point (same for both cameras)
@@ -67,8 +105,20 @@ bool Triangulation::triangulation(
         std::vector<vec3> &points_3d,         /// output: reconstructed 3D points
         mat3 &R,   /// output: recovered rotation of 2nd camera (used for updating the viewer and visual inspection)
         vec3 &t    /// output: recovered translation of 2nd camera (used for updating the viewer and visual inspection)
-) const
-{
+
+        ) const
+
+
+{    for (int i = 0; i < points_0.size(); i++){
+        if (points_0[i].size() !=points_1.size() && points_0[i].z ==1 && points_1[i].z==1 && points_0[i].size() ==3  && points_1[i].size()==3){
+            continue;
+        }
+        else{
+            std::cout << "Input files are not correct. \n";
+            return false;
+        }
+    }
+
     /// NOTE: there might be multiple workflows for reconstructing 3D geometry from corresponding image points.
     ///       This assignment uses the commonly used one explained in our lecture.
     ///       It is advised to define a function for each sub-task. This way you have a clean and well-structured
@@ -161,19 +211,17 @@ bool Triangulation::triangulation(
     /// ----------- dynamic-size matrices
 
     /// define a non-fixed size matrix
-    Matrix<double> W(2, 3, 0.0); // all entries initialized to 0.0.
+    Matrix<double> W2(2, 3, 0.0); // all entries initialized to 0.0.
 
     /// set its first row by a 3D vector (1.1, 2.2, 3.3)
-    W.set_row({ 1.1, 2.2, 3.3 }, 0);   // here "{ 1.1, 2.2, 3.3 }" is of type 'std::vector<double>'
+    W2.set_row({ 1.1, 2.2, 3.3 }, 0);   // here "{ 1.1, 2.2, 3.3 }" is of type 'std::vector<double>'
 
     /// get the last column of a matrix
-    std::vector<double> last_column = W.get_column(W.cols() - 1);
+    std::vector<double> last_column = W2.get_column(W2.cols() - 1);
 
     // TODO: delete all above demo code in the final submission
 
-    //--------------------------------------------------------------------------------------------------------------
-    // implementation starts ...
-      //************************************ Normalization of points for image1 and image2 *************************************************** //
+//******************************** Normalization of points: image1 and image2 ****************************************//
 
     float x_1= 0; // image 1
     float y_1 = 0;
@@ -184,41 +232,48 @@ bool Triangulation::triangulation(
     std::vector<float> x_m2;
     std::vector<float> y_m2;
 
-    for (vec3 p1: points_0){
+    for (vec3 p1: points_0){ //sum of all x and y:  from points_0 dataset
         x_1 = x_1 + p1[0];
         y_1 = y_1 + p1[1];
     }
 
-    for (vec3 p2: points_1){
+    for (vec3 p2: points_1){ //sum of all x and y:  from points_1 dataset
         x_2 = x_2 + p2[0];
         y_2 = y_2 + p2[1];
     }
 
-    vec2 centroid1 = {x_1 / points_0.size(),y_1 / points_0.size() };
-    vec2 centroid2 = {x_2 / points_0.size(),y_2 / points_0.size() };
+    vec3 centroid1 = {x_1 / points_0.size(),y_1 / points_0.size(),1 }; // centroid computation for the first camera
+    vec3 centroid2 = {x_2 / points_1.size(),y_2 / points_1.size(),1 }; // centroid computation for the second camera
 
     float s1=0;
     float s2=0;
 
-    for (int i=0; i<points_0.size(); i++) {
+    for (int i=0; i<points_0.size(); i++) { // computation of scale factor for each camera
 
-        float xmean1 = points_0[i]._array[0] - centroid1[0];
-        float ymean1 = points_0[i]._array[1] - centroid1[1];
+        float xmean1 = points_0[i].x - centroid1[0];
+        float ymean1 = points_0[i].y - centroid1[1];
         x_m1.push_back(xmean1);
         y_m1.push_back(ymean1);
-        s1 = s1 + (x_m1[i]*x_m1[i]+y_m1[i]*y_m1[i]);
+        s1 = s1 + sqrt(x_m1[i]*x_m1[i]+y_m1[i]*y_m1[i]);
 
-        float xmean2 = points_1[i]._array[0] - centroid2[0];
-        float ymean2 = points_1[i]._array[1] - centroid2[1];
+        float xmean2 = points_1[i].x - centroid2[0];
+        float ymean2 = points_1[i].y - centroid2[1];
         x_m2.push_back(xmean2);
         y_m2.push_back(ymean2);
-        s2 = s2 + (x_m2[i]*x_m2[i]+y_m2[i]*y_m2[i]);
+        s2 = s2 + sqrt(x_m2[i]*x_m2[i]+y_m2[i]*y_m2[i]);
     }
-    s1 = sqrt(2)/sqrt(s1); // scale factor for image 1 : 0.000648619
-    s2 = sqrt(2)/sqrt(s2); // scale factor for image 2 : 0.000746701
+    s1 = s1/points_0.size();
+    s2 = s2/ points_1.size();
 
-    mat3 T1; //Transformation matrix image 1
-    mat3 T2; // Transformation matrix image 2
+    s1 = sqrt(2)/(s1); // scale factor for image 1 : 0.00846696
+    s2 = sqrt(2)/(s2); // scale factor for image 2 : 0.00983743
+
+
+    std::vector<vec3> points1; // normalized points image 1 // type: float
+    std::vector<vec3> points2; // normalized points image 2 // type: float
+
+    mat3 T1; //Transformation matrix image 1 //type float
+    mat3 T2; // Transformation matrix image 2 //type float
 
     T1.set_row(0,vec3(s1, 0 , -(s1*centroid1[0])));
     T1.set_row(1,vec3(0, s1 , -(s1*centroid1[1])));
@@ -228,98 +283,202 @@ bool Triangulation::triangulation(
     T2.set_row(1,vec3(0, s2 , -(s2*centroid2[1])));
     T2.set_row(2,vec3(0, 0 , 1));
 
-
-    vec3 a = T1.operator*({points_0[0].x,points_0[0].y,points_0[0].z});
-    std::cout << T1 << std::endl << points_0[0].x <<" "<< points_0[0].y<< " "<<points_0[0].z<< std::endl <<a << std::endl;
-
-    std::vector<vec3> points1;
-    std::vector<vec3> points2;
+//
+//    for (int i =0; i<points_0.size(); i++){
+//        points1.push_back((points_0[i] - centroid1)*s1);
+//        points2.push_back((points_1[i] - centroid2)*s2);
+//    }
 
     for (int i =0; i < points_0.size(); i++){
+
         points1.push_back(T1.operator*({points_0[i].x,points_0[i].y,points_0[i].z}));
         points2.push_back(T2.operator*({points_1[i].x,points_1[i].y,points_1[i].z}));
     }
-    std::cout << points1 << std::endl;
+ std::cout<< points1<<std::endl;
+//********************************************   Foundamental matrix construction ************************************//
 
-    Matrix<double>A(points1.size(),9);
-    for (int i = 0; i < points1.size(); i++){
-        auto a = points1[i].x * points2[i].x;
-        auto b = points1[i].y * points2[i].x;
-        auto c = points2[i].x;
+    Matrix<double> W1(points_0.size(),9);
+    for (int i = 0; i < points_0.size(); i++){
+        auto a = points1[i].x  * points2[i].x;
+        auto b1 = points1[i].y * points2[i].x;
+        auto c1 = points2[i].x;
         auto d = points1[i].x * points2[i].y;
         auto e = points1[i].y * points2[i].y;
         auto f = points2[i].y;
         auto g = points1[i].x;
         auto h = points1[i].y;
-        A.set(i,0,a); A.set(i,1,b); A.set(i,2,c); A.set(i,3,d);
-        A.set(i,4,e); A.set(i,5,f); A.set(i,6,g); A.set(i,7,h);
-        A.set(i,8,1);
+        W1.set(i,0,a); W1.set(i,1,b1); W1.set(i,2,c1); W1.set(i,3,d);
+        W1.set(i,4,e); W1.set(i,5,f); W1.set(i,6,g); W1.set(i,7,h);
+        W1.set(i,8,1);
     }
-    std::cout << A << std::endl;
-
-    Matrix<double> AT = A.transpose();
-    Matrix<double> ATA = AT*A;
-    std::cout << "AT * A" << AT*A << std::endl;
 
 
+    // Compute the SVD decomposition of W1
 
-    Matrix<double> U(ATA.rows(), ATA.rows(), 0.0);   // initialized with 0s
-    Matrix<double> S(ATA.rows(), ATA.cols(), 0.0);   // initialized with 0s
-    Matrix<double> V(ATA.cols(), ATA.cols(), 0.0);   // initialized with 0s
+    Matrix<double> U(W1.rows() , W1.rows() , 0.0);   // initialized with 0s
+    Matrix<double> S(W1.rows() , W1.cols() , 0.0);   // initialized with 0s
+    Matrix<double> V(W1.cols() , W1.cols() , 0.0);   // initialized with 0s
 
-    // Compute the SVD decomposition of A
-    svd_decompose(ATA, U, S, V);
-    std::cout << "U" << U << std::endl;
-    std::cout << "S" << S << std::endl;
-    std::cout << "V" << V << std::endl;
+    svd_decompose(W1, U, S, V);
 
-    auto count = 0;
-    auto vcol = V.get_column(V.cols()-1);
-    for (int i = 0; i < 9; i=i+3){
-        std::vector<double> v1 = V.get_column(V.cols()-1);
-        vec3 v = {float(v1[i+0]),float(v1[i+1]),float(v1[i+2])};
-        F.set_row(count,v);
-        count++;
+    std::vector<double> f = V.get_column(V.cols()-1);
+
+    Matrix<double> Fq (3,3,0.0);
+    Fq.set_row({f[0], f[1], f[2]}, 0);
+    Fq.set_row({f[3], f[4], f[5]}, 1);
+    Fq.set_row({f[6], f[7], f[8]}, 2);
+
+    Matrix<double> Uf(Fq.rows(), Fq.rows(), 0.0);   // initialized with 0s
+    Matrix<double> Sf(Fq.rows(), Fq.cols(), 0.0);   // initialized with 0s
+    Matrix<double> Vf(Fq.cols(), Fq.cols(), 0.0);   // initialized with 0s
+    svd_decompose(Fq, Uf, Sf, Vf);
+
+    Sf.set(Sf.rows()-1,Sf.cols()-1,0.0); //enforce rank-2 constraint
+
+    auto Ftemp =Uf*Sf*transpose(Vf); // SVD derived from equations USVtranspose
+
+    F = to_mat3(Ftemp);
+    F = transpose(T2)*F*T1;   //de-normalization
+    F = F/(F(2,2));  //set the last column's element =1 : second constraint
+
+    std::cout << "Foundamental matrix F = " << std::endl << F << std::endl;
+
+    //matrix K same for both images
+    mat3 K;
+    K.set_row (0, vec3(fx, 0, cx));
+    K.set_row(1, vec3(0, fy,cy));
+    K.set_row(2, vec3(0, 0, 1));
+
+    mat3 Etemp;
+    Etemp = transpose(K)*F*K; // transpose K * E * K
+
+    Matrix<double> Ue(3, 3, 0.0);   // initialized with 0s
+    Matrix<double> Se(3, 3, 0.0);   // initialized with 0s
+    Matrix<double> Ve(3, 3, 0.0);   // initialized with 0s
+
+    auto E = to_Matrix(Etemp);
+    svd_decompose(E, Ue, Se, Ve );
+
+    std::cout << "Essential matrix E = " << std::endl <<  E << std::endl;
+
+// ****************************************** PART 2 retrieve R and t *********************************************** //
+    mat3 W;
+    W.set_row(0,vec3(0,-1,0));
+    W.set_row(1,vec3(1,0,0));
+    W.set_row(2,vec3(0,0,1));
+//
+//    mat3 Z;
+//    Z.set_row(0,vec3(0,1,0));
+//    Z.set_row(1,vec3(-1,0,0));
+//    Z.set_row(2,vec3(0,0,0));
+
+    std::vector<double> t_temp;
+    auto t1 = t;
+    auto t2 = t;
+    auto R1 = R;
+    auto R2 = R;
+
+    t_temp = Ue.get_column(Ue.cols()-1); //vec3 float
+    t1 = vec3(t_temp.data()); // read it from the std::vector // +u3: positive value of vector t
+    t2 = - vec3(t_temp.data()); // read it from the std::vector // -u3: negative value of vector t
+
+    R1 = to_mat3(Ue)*W*transpose(to_mat3(Ve))*determinant(to_Matrix(to_mat3(Ue)*W*transpose(to_mat3(Ve)))); //mat3 float : first case where R = det(UWVT)UWVT
+    R2 =  to_mat3(Ue)*transpose(W)*transpose(to_mat3(Ve))*determinant(to_Matrix(to_mat3(Ue)*transpose(W)*transpose(to_mat3(Ve)))); //mat3 float: second case where R = det(UWVTT)UWTVT
+
+    std::vector<vec3> casest = {t1,t2,t1,t2};
+    std::vector<mat3> casesR = {R1,R1,R2,R2};
+    auto thers1 = determinant(R1);
+    auto thers2 = determinant(R2);
+
+    mat34 Rt0;     //set M for the origin R=I t=0
+    Rt0.load_identity(1);
+    vec3 t3(0,0,0);
+    Rt0.set_col(3,t3);
+
+    auto points_3d_1 = points_3d;
+    auto points_3d_2 = points_3d;
+    auto points_3d_3 = points_3d;
+    auto points_3d_4 = points_3d;
+
+//  *************************************************** Triangulation of points **************************************//
+    for (int i = 0; i < points_0.size(); i++) {
+        auto Rt1 = Rt_selection(R1,t1);
+        points_3d_1.push_back(triangulation1(points_0[i], points_1[i], K, Rt1, Rt0));
     }
-    std::cout << "F" << std::endl <<  F << std::endl;
-
-    auto Fm = to_Matrix(F);
-
-    Matrix<double> u(Fm.rows(), Fm.rows(), 0.0);   // initialized with 0s
-    Matrix<double> s(Fm.cols(), Fm.rows(), 0.0);   // initialized with 0s
-    Matrix<double> v(Fm.cols(), Fm.cols(), 0.0);   // initialized with 0s
-    svd_decompose(Fm, u, s, v);
-    s.set(s.rows()-1,s.cols()-1,0.0);
-    std::cout << "U" << u << std::endl;
-    std::cout << "S" << s << std::endl;
-    std::cout << "V" << v << std::endl;
-    std::cout << "Fm" << std::endl <<  Fm << std::endl;
-
-    auto Ftemp = u*s*v;
-    std::cout << "Ftemp" << std::endl <<  Ftemp << std::endl;
-
-    auto Ft = to_mat3(Ftemp);
-    count=0;
-    for (int i = 0; i < 3; i++){
-        F.set_row(i,Ft.row(i));
+    for (int i = 0; i < points_0.size(); i++) {
+        auto Rt2 = Rt_selection(R1,t2);
+        points_3d_2.push_back(triangulation1(points_0[i], points_1[i], K, Rt2, Rt0));
     }
-    std::cout << "F" << std::endl <<  F << std::endl;
+    for (int i = 0; i < points_0.size(); i++) {
+        auto Rt3 = Rt_selection(R2,t1);
+        points_3d_3.push_back(triangulation1(points_0[i], points_1[i], K, Rt3, Rt0));
+    }
+    for (int i = 0; i < points_0.size(); i++) {
+        auto Rt4 = Rt_selection(R2,t2);
+        points_3d_4.push_back(triangulation1(points_0[i], points_1[i], K, Rt4, Rt0));
+    }
 
+//  *********************************** Estimation of relative pose of the views *************************************//
+    int positivez1=0;
+    int positivez2=0;
+    int positivez3=0;
+    int positivez4=0;
+    std::vector<std::vector<vec3>> points;
+    points.push_back(points_3d_1);
+    points.push_back(points_3d_2);
+    points.push_back(points_3d_3);
+    points.push_back(points_3d_4);
+
+    for(int i=0; i<points_3d_1.size(); i++){ // check for points with positive z values
+        vec3 Rtp=R1*points_3d_1[i] + t1;
+        if (points_3d_1[i].z > 0 and Rtp.z > 0){
+            positivez1++ ;
+        }
+    }
+    for(int i=0; i<points_3d_2.size(); i++){ // check for points with positive z values
+        vec3 Rtp=R1*points_3d_2[i] + t2;
+        if (points_3d_2[i].z > 0 and Rtp.z > 0){
+            positivez2++ ;
+        }
+    }
+    for(int i=0; i<points_3d_3.size(); i++){ // check for points with positive z values
+        vec3 Rtp=R2*points_3d_3[i] + t1;
+        if (points_3d_3[i].z > 0 and Rtp.z > 0){
+            positivez3++ ;
+        }
+    }
+    for(int i=0; i<points_3d_4.size(); i++){ // check for points with positive z values
+        vec3 Rtp=R2*points_3d_4[i] + t2;
+        if (points_3d_4[i].z > 0 and Rtp.z > 0){
+            positivez4++ ;
+        }
+    }
+
+    std::vector<int> z_coords = {positivez1,positivez2,positivez3,positivez4};
+    int max = z_coords[0];
+    int counter0=0;
+    int counter1=0;
+    for (auto const& i : z_coords){
+        if (i >= max){
+            max = i;
+            counter1 = counter0;
+        }
+        counter0++;
+    }
+
+    points_3d = points[counter1];
+    R = casesR[counter1];
+    t = casest[counter1];
+    std::vector<std::string> cases = {"R = det((U W V^T)(U W V^T)), t = +u3","R = det((U W V^T)(U W V^T)), t = -u3","R = det((U W^T V^T)(U W^T V^T)), t = +u3","R = det((U W^T V^T)(U W^T V^T)), t = -u3"};
+    std::cout<< "Rotation matrix R "<< R << " "<< " Determinant of matrix R" << std::endl;
+    std::cout<< " Vector t " << t << std::endl;
+    std::cout<<" Points with positive z values" << " "<< z_coords[counter1] << std::endl;
+    std::cout<< " Case number " << counter1+1 << " -> "<< cases[counter1]<< std::endl;
 
     // TODO: check if the input is valid (always good because you never known how others will call your function).
 
-    // TODO: Estimate relative pose of two views. This can be subdivided into
-    //      - estimate the fundamental matrix F;
-    //      - compute the essential matrix E;
-    //      - recover rotation R and t.
-
-    // TODO: Reconstruct 3D points. The main task is
-    //      - triangulate a pair of image points (i.e., compute the 3D coordinates for each corresponding point pair)
 
     // TODO: Don't forget to
-    //          - write your recovered 3D points into 'points_3d' (the viewer can visualize the 3D points for you);
-    //          - write the recovered relative pose into R and t (the view will be updated as seen from the 2nd camera,
-    //            which can help you to check if R and t are correct).
     //       You must return either 'true' or 'false' to indicate whether the triangulation was successful (so the
     //       viewer will be notified to visualize the 3D points and update the view).
     //       However, there are a few cases you should return 'false' instead, for example:
